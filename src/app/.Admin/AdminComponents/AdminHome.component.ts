e65e3080e9ba9af8d6fac35fe18ffa64156cc9de
Dawit A <dndawita@gmail.com>
2023-07-28 16:59:01 -0400

    added cloudinary

--- src/app/.Admin/AdminComponents/AdminHome.component.ts
+++ src/app/.Admin/AdminComponents/AdminHome.component.ts
@@ -21,7 +21,7 @@ import { Auth } from '@angular/fire/auth';
                 cloud storage to upload new photos. <br>
                 Click the three dots on your uploaded image and click copy url to get the image url. <br>
                 Then use those image urls when creating a new event or gallery. <br> <br>
-                Cloudinary Login: debretsionkidistmariamwekidusg@gmail.com Pw: Fo@512LR^6&fFDzzcLnHeoy28XlSiW
+                Cloudinary Login: debretsionkidistmariamwekidusg@gmail.com Pw: {{ Password }} 
               </p>
             </div>
           </div>
