63b79a91e8370b3c614bd3d40738f40f7331f0eb
Dawit Alemu <106638403+dawitalemu4@users.noreply.github.com>
2025-02-14 19:46:43 -0600

    Update AmharicHeader.component.ts

--- src/app/.GlobalComponents/AmharicHeader.component.ts
+++ src/app/.GlobalComponents/AmharicHeader.component.ts
@@ -29,11 +29,6 @@ import { Router } from "@angular/router";
             <a (click)="navigateTo('/Events')">ክስተቶች</a>
             <a id='Live' (click)="navigateTo('/Live')">ቀጥታ ሥርጭት</a>
             <a (click)="navigateTo('/Gallery')">ፎቶግራፎች</a>
-            <a id="NavbarListMember" mat-button [matMenuTriggerFor]="memberMenu">አባላት</a>
-            <mat-menu #memberMenu="matMenu">
-              <a mat-menu-item href="https://www.zeffy.com/en-US/ticketing/1efa851d-a182-4c79-9e8e-f436c35178b7">ያገቡ</a>
-              <a mat-menu-item href="https://www.zeffy.com/en-US/ticketing/8b37c24f-a593-4754-a691-d09a7b1a2628">ያላገቡ</a>
-            </mat-menu>
             <a href="https://www.zeffy.com/ticketing/8b37c24f-a593-4754-a691-d09a7b1a2628">አባላት</a>
             <a (click)="navigateTo('/Donate')">ለግሱ</a> <!-- አባልነት -->
             <a (click)="navigateTo('/Contact')">አግኙን</a>
